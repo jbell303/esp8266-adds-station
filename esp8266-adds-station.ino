60f312769485e69940949aa364fcfad499dc2c27
James Bell <jbell303@gmail.com>
2020-05-20 17:13:15 -0700

    add mDNS, autoconnect

--- esp8266-adds-station.ino
+++ esp8266-adds-station.ino
@@ -1,15 +1,63 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoJson.h>
+#include <AutoConnect.h>
+#include <Adafruit_NeoPixel.h>
 
-/*
- * Sketch: esp8266-adds-station
- * Intended to be run on an ESP8266
- * 
- * Uses station mode on ESP8266
- */
+// ADDS 
+const char* host = "aviationweather.gov";
+const int httpsPort = 443;
+const char* fingerprint = "07e32864918f4238a0542a2ccdc17c98798ca1d8";
+String identifier = "KNFL";
+String url = "/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true&stationString=";
 
-String header = "HTTP/1.1 200 OK\r\nContent-Type: JSON\r\n\r\n";
-String textHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+String flight_category = "";
+String visibility = "";
+String sky_cover = "";
+String cloud_base = "";
+int wind_speed = 0;
+String ceiling = "";
 
-String html_1 = R"=====(
+ESP8266WebServer server(80);
+AutoConnect Portal(server);
+
+// intitialize timer for auto updates
+unsigned long timer;
+bool timerActive;
+
+// NeoPixels
+#define LED_PIN    5
+#define LED_COUNT  100
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+// button
+const int buttonPin = 13; // button for manual toggle
+uint32_t green_strip = 0; // for color evaluation
+uint32_t red_strip = 0;
+
+void setFlyingWeatherLights () {
+  strip.clear();
+  strip.fill(strip.Color(255, 255, 255), 0, 25); // left strip
+  strip.fill(strip.Color(0, 255, 0), 25, 25); // top strip
+  strip.fill(strip.Color(255, 255, 255), 50, 24); // right strip
+  strip.fill(strip.Color(0, 0, 0), 74, 26); // bottom strip
+  strip.show();
+}
+
+void setDrinkingWeatherLights () {
+  strip.clear();
+  strip.fill(strip.Color(255, 255, 255), 0, 25);
+  strip.fill(strip.Color(0, 0, 0), 25, 25);
+  strip.fill(strip.Color(255, 255, 255), 50, 24);
+  strip.fill(strip.Color(255, 0, 0), 74, 26);
+  strip.show();
+}
+
+// html and javascript
+const String html_1 = R"=====(
 <!DOCTYPE html>
 <html>
   <head>
@@ -23,75 +71,105 @@ String html_1 = R"=====(
    </style>
 
 <script>
+  // set the button text to a pending status
+  // read the station id from the form and use it in our request URL
   function fetchWeather() {
     document.getElementById("submit_button").value = "Fetching weather...";
     station_id = document.getElementById("identifier").value;
     ajaxLoad(station_id);
   }
 
-var ajaxRequest = null;
-if (window.XMLHttpRequest) { ajaxRequest = new XMLHttpRequest(); }
+  // initialize ajax request object
+  var ajaxRequest = null;
+  if (window.XMLHttpRequest) { ajaxRequest = new XMLHttpRequest(); }
                            { ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP"); }
 
-function ajaxLoad(station_id) {
-  if (!ajaxRequest) {
-    alert("AJAX is not supported.");
-    return;
-  }
-
-  ajaxRequest.open("POST", "station_id=" + station_id, true);
-  ajaxRequest.onreadystatechange = function () {
-    if (ajaxRequest.readyState == 4 && ajaxRequest.status == 200) {
-      var response = JSON.parse(ajaxRequest.responseText);
-      document.getElementById("identifier").value = response.identifier;
-      document.getElementById("flight_category").innerHTML = "Flight Category: " + response.flight_category;
-      document.getElementById("ceiling").innerHTML = "Ceiling: " + response.ceiling;
-      document.getElementById("wind_speed").innerHTML = "Wind speed: " + response.wind_speed;
-      document.getElementById("submit_button").value = "Fetch Weather";
-      document.getElementById("toggle_button").value = (response.isDrinkingWeather ? "Set Flying Weather" : "Set Drinking Weather");
+  // send a POST request to the server with station ID
+  // parse the response and display the values in HTML
+  function ajaxLoad(station_id) {
+    if (!ajaxRequest) {
+      alert("AJAX is not supported.");
+      return;
     }
-  }
-  ajaxRequest.send();
-}
 
-function toggleLed () {
-  var button_text = document.getElementById("toggle_button").value;
-  if (button_text == "Set Drinking Weather") {
-    document.getElementById("toggle_button").value = "Setting Drinking Weather";
-    ajaxLED('DRINKING');
-  } else {
-    document.getElementById("toggle_button").value = "Setting Flying Weather";
-    ajaxLED('FLYING');
-  }
-}
-
-function ajaxLED(ajaxURL) {
-  if (!ajaxRequest) {
-    alert("AJAX is not supported.");
-    return;
-  }
-
-  ajaxRequest.open("GET", ajaxURL, true);
-  ajaxRequest.onreadystatechange = function () {
-    if (ajaxRequest.readyState == 4 && ajaxRequest.status == 200) {
-      var ajaxResult = ajaxRequest.responseText;
-      if (ajaxResult == "Flying Weather") {
-        document.getElementById("toggle_button").value = "Set Drinking Weather";
-      } else if (ajaxResult == "Drinking Weather") {
-        document.getElementById("toggle_button").value = "Set Flying Weather";
+    ajaxRequest.open("POST", "station_id/", true);
+    ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    ajaxRequest.send("station_id=" + station_id);
+    ajaxRequest.onreadystatechange = function () {
+      if (ajaxRequest.readyState == 4 && ajaxRequest.status == 200) {
+        var response = JSON.parse(ajaxRequest.responseText);
+        document.getElementById("identifier").value = response.identifier;
+        document.getElementById("flight_category").innerHTML = "Flight Category: " + response.flight_category;
+        document.getElementById("ceiling").innerHTML = "Ceiling: " + response.ceiling;
+        document.getElementById("wind_speed").innerHTML = "Wind speed: " + response.wind_speed;
+        document.getElementById("submit_button").value = "Fetch Weather";
+        document.getElementById("toggle_button").value = (response.isDrinkingWeather ? "Set Flying Weather" : "Set Drinking Weather");
       }
     }
+    ajaxRequest.send();
   }
-  ajaxRequest.send();
-}
+
+  // manually toggle flying weather and drinking weather lights
+  // change the button text based on the desired state
+  // send the LED toggle request to the server
+  function toggleLed () {
+    var button_text = document.getElementById("toggle_button").value;
+    if (button_text == "Set Drinking Weather") {
+      document.getElementById("toggle_button").value = "Setting Drinking Weather";
+      ajaxLED('DRINKING');
+    } else {
+      document.getElementById("toggle_button").value = "Setting Flying Weather";
+      ajaxLED('FLYING');
+    }
+  }
+
+  function ajaxLED(ajaxURL) {
+    if (!ajaxRequest) {
+      alert("AJAX is not supported.");
+      return;
+    }
+
+    ajaxRequest.open("GET", ajaxURL, true);
+    ajaxRequest.onreadystatechange = function () {
+      if (ajaxRequest.readyState == 4 && ajaxRequest.status == 200) {
+        var ajaxResult = ajaxRequest.responseText;
+        if (ajaxResult == "Flying Weather") {
+          document.getElementById("toggle_button").value = "Set Drinking Weather";
+        } else if (ajaxResult == "Drinking Weather") {
+          document.getElementById("toggle_button").value = "Set Flying Weather";
+        }
+      }
+    }
+    ajaxRequest.send();
+  }
+
+  // request the local IP address from the server
+  function getIpAddress() {
+    if (!ajaxRequest) {
+      alert("AJAX is not supported.");
+      return;
+    }
+
+    // display the local IP address in HTML
+    ajaxRequest.open("GET", "ip_address", true);
+    ajaxRequest.onreadystatechange = function () {
+      if (ajaxRequest.readyState == 4 && ajaxRequest.status == 200) {
+        var ajaxResult = ajaxRequest.responseText;
+        console.log(ajaxResult);
+        document.getElementById("ip_address").innerHTML = ajaxResult;
+      }
+    }
+    ajaxRequest.send();
+  }
 </script>
 
-  <title>esp8266 ADDS Station</title>
+  <title>Saints Weather Station</title>
 </head>
 
-<body>
+<body onload="getIpAddress()">
   <div id='main'>
-    <h2>esp8266 ADDS Station</h2>
+    <h2>Saints Weather Station</h2>
+    <p id="ip_address"></p>
     <p id="flight_category">Flight Category: </p>
     <p id="ceiling">Ceiling: </p>
     <p id="wind_speed">Winds: </p>
@@ -112,45 +190,6 @@ function ajaxLED(ajaxURL) {
 </html>
 
 )=====";
-
-#include <ESP8266WiFi.h>
-#include <ArduinoJson.h>
-#include <Adafruit_NeoPixel.h>
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h>
-//#include <ESP8266mDNS.h>
-
-// WIFI
-const char* ssid =  "JamesBell'sNetwork_2.4";
-const char* password = "Ju51!3@Io";
-
-WiFiServer server(80);
-
-// ADDS
-const char* host = "aviationweather.gov";
-const int httpsPort = 443;
-const char* fingerprint = "29f037bace863c943c3bdf2ce5303a243eaef491";
-String identifier = "KNFL";
-String url = "/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true&stationString=";
-
-String flight_category = "";
-String visibility = "";
-String sky_cover = "";
-String cloud_base = "";
-int wind_speed = 0;
-String ceiling = "";
-
-// NeoPixels
-#define LED_PIN    5
-#define LED_COUNT    102
-
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
-// button
-const int buttonPin = 13;
-uint16_t n = 0;
-uint32_t green_strip = 0;
-uint32_t red_strip = 0;
 
 String getValueforTag(String line, String tag) {
   String matchString;
@@ -176,7 +215,7 @@ String getValueforParameter(String line, String param) {
 }
 
 bool isDrinkingWeather(String flight_category, int wind_speed, String ceiling) {
-  if (flight_category == "IFR" || ceiling.toInt() < 3000 || wind_speed > 25) {
+  if (flight_category == "IFR" || flight_category == "LIFR" || ceiling.toInt() < 3000 || wind_speed > 25) {
     setDrinkingWeatherLights();
     return true;
   }
@@ -187,8 +226,6 @@ bool isDrinkingWeather(String flight_category, int wind_speed, String ceiling) {
 void fetchWeather() {
   // Use WiFiClientSecure to create TLS connection
   WiFiClientSecure client;
-//  BearSSL::WiFiClientSecure client;
-//  client.setInsecure();
 
   Serial.printf("\nConnecting to %s ...", host);
   Serial.print("\nFetching weather for...");
@@ -203,21 +240,9 @@ void fetchWeather() {
     Serial.println("connection failed");
     return;
   }
-//   if (!client.connect(host, 80)) {
-//    Serial.println("connection failed");
-//    return;
-//  }
+  
   Serial.println("connected");
   
-  // verify fingerprint from host
-//  if (client.verify(fingerprint, host)) {
-//     Serial.println("certificate matches");
-//      
-//  } else { // close the connection if certificate does not match
-//     Serial.println("certificate doesn't match");
-//     client.stop();
-//     Serial.println("\n[Disconnected]");
-//  }
   Serial.println("[Sending a request]");
      client.print(String("GET ") + url + identifier + " HTTP/1.1\r\n" +
                   "Host: " + host + "\r\n" +
@@ -228,7 +253,8 @@ void fetchWeather() {
 
      // debug response to console
      Serial.println("[Response:]");
-     
+
+     // parse the server response
      while (client.connected() || client.available()){
         if (client.available()){
           String line = client.readStringUntil('\r');
@@ -264,7 +290,9 @@ void fetchWeather() {
             wind_speed = value.toInt();
           }
         }
-     } // close the connection
+     } 
+     
+     // close the connection
      Serial.println("visibility: " + visibility + ", flight category: " + flight_category);
      Serial.println("sky cover: " + sky_cover + ", cloud base: " + cloud_base);
      Serial.println("ceiling: " +  (ceiling.toInt() >= 50000 ? "None" : ceiling + " feet"));
@@ -274,160 +302,146 @@ void fetchWeather() {
      Serial.println("[Disconnected]");
 }
 
-void connectToWifi(){
-  // connect to wifi
-  Serial.println("connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+void handleRoot() {
+  server.send(200, "text/html", html_1);
+}
+
+void handleGetIp() {
+  server.send(200, "text/plain", WiFi.localIP().toString());
+}
+
+void handleDrinkingToggle() {
+  timerActive = false;
+  setDrinkingWeatherLights();
+  Serial.println("setting drinking weather...");
+  server.send(200, "text/plain", "Drinking Weather");
+}
+
+void handleFlyingToggle() {
+  timerActive = false;
+  setFlyingWeatherLights();
+  Serial.println("setting flying weather...");
+  server.send(200, "text/plain", "Flying Weather");
+}
+
+void handleWeatherForm() {
+   if (server.method() != HTTP_POST) {
+    server.send(405, "text/plain", "Method Not Allowed");
+    
+  } else {
+
+    // fetch weather from ADDS server
+    if (server.argName(0) == "station_id") {
+      identifier = server.arg(0);
+      Serial.println("fetching weather for: " + identifier); 
+      fetchWeather();
+      
+      // serialize response to JSON 
+      const size_t capacity = JSON_OBJECT_SIZE(7);
+      DynamicJsonDocument doc(capacity);
+    
+      doc["identifier"] = identifier;
+      doc["flight_category"] = flight_category;
+      doc["ceiling"] = (ceiling.toInt() >= 50000 ? "None" : ceiling + " feet");
+      doc["wind_speed"] = String(wind_speed) + " knots";
+      doc["isDrinkingWeather"] = isDrinkingWeather(flight_category, wind_speed, ceiling);
+
+      serializeJson(doc, Serial);
+      String docString;
+      serializeJson(doc, docString);
+
+      // send response to client
+      server.send(200, "JSON", docString);
+    }
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // connect using wifimanager
-//  WiFiManager wifiManager;
-//  wifiManager.autoConnect("WeatherStation");
-  
-  Serial.println("Connected.");
-
-  //try mDNS
-//  if (!MDNS.begin("weatherstation"))  { Serial.println("Error setting up mDNS responder!"); }
-//                                      { Serial.println("mDNS responder started!"); }
-  
-  // start a server
-  server.begin();
-  Serial.println("Server started"); 
 }
 
-void setFlyingWeatherLights () {
-  strip.clear();
-  strip.fill(strip.Color(2, 2, 2), 0, 25);
-  strip.fill(strip.Color(0, 0, 0), 25, 26);
-  strip.fill(strip.Color(2, 2, 2), 51, 25);
-  strip.fill(strip.Color(0, 150, 0), 76, 26);
-  strip.show();
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
 }
 
-void setDrinkingWeatherLights () {
-  strip.clear();
-  strip.fill(strip.Color(2, 2, 2), 0, 25);
-  strip.fill(strip.Color(150, 0, 0), 25, 26);
-  strip.fill(strip.Color(2, 2, 2), 51, 25);
-  strip.fill(strip.Color(0, 0, 0), 76, 26);
-  strip.show();
-}
-
-unsigned long timer;
-bool timerActive;
-
-void setup() {
+void setup(void) {
+  // set button
   pinMode(buttonPin, INPUT_PULLUP);
+  
+  // initialize serial port
   Serial.begin(115200);
-
-  String request = "";
+  Serial.println("");
 
   // initialize NeoPixels
   strip.begin();
   strip.show();
-//  strip.setBrightness(50);
+  
+  // initialize response handlers
+  server.on("/", handleRoot);
 
-  // connect to wifi
-  connectToWifi();
+  server.on("/station_id/", handleWeatherForm);
 
-  // start the timer
-  timerActive = true;
+  server.on("/DRINKING", handleDrinkingToggle);
+
+  server.on("/FLYING", handleFlyingToggle);
+
+  server.on("/ip_address", handleGetIp);
+
+  server.onNotFound(handleNotFound);
+
+  // start http server
+  Portal.begin();
+  Serial.println("HTTP server started: " +
+          WiFi.localIP().toString());
+
+  // start MDNS server
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
 }
 
-void loop() {
-
-  // toggle button
+void loop(void) {
+  
+  // if the button is pressed, manually toggle the 
+  // flying and drinking lights
   if (digitalRead(buttonPin) == LOW) {
     Serial.println("Button pressed...");
+    
+    // stop weather updates
     timerActive = false;
-    green_strip = strip.getPixelColor(76);
-    red_strip = strip.getPixelColor(25);
-//    Serial.print("Color: ");
-//    Serial.println(green_strip);
-//    Serial.println(red_strip);
-//    n = strip.numPixels();
-//    Serial.print("n: ");
-//    Serial.println(n);
+
+    // toggle the drinking and flying weather lights
+    green_strip = strip.getPixelColor(25); // 1st LED top strip
+    red_strip = strip.getPixelColor(74); // 1st LED bottom strip
+    
     if (red_strip == 0 && green_strip == 0){
       Serial.println("Setting drinking weather...");
       setDrinkingWeatherLights();
-    } else if (red_strip == strip.Color(150, 0, 0)){
+    } else if (red_strip == strip.Color(255, 0, 0)){
       Serial.println("Setting flying weather...");
       setFlyingWeatherLights();
-    } else if (green_strip == strip.Color(0, 150, 0)){
+    } else if (green_strip == strip.Color(0, 255, 0)){
       Serial.println("Clearing strip...");
       strip.clear();
       strip.show();
     }
+    // delay a short time to avoid invoking twice
     delay(500);
   }
 
+  // check the weather once every minute
   if (timerActive && (millis() % 60000) == 0) {
     fetchWeather();
   }
-  
-  // check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
 
-  // read the first line of the request
-  String request = client.readStringUntil('\r');
-
-  Serial.print("request: ");
-  Serial.println(request);
-
-  if (request.indexOf("station_id") > 0) {
-    // fetch weather for requested station id
-    int idx = request.indexOf("station_id=");
-    if (idx != -1) {
-      int paramStart = idx + 11; // station_id=
-      int paramEnd = paramStart + 4;
-      identifier = request.substring(paramStart, paramEnd);
-    }
-    timerActive = true;
-    fetchWeather();
-
-    // JSON 
-    const size_t capacity = JSON_OBJECT_SIZE(7);
-    DynamicJsonDocument doc(capacity);
-    
-    // serialize json response
-    doc["identifier"] = identifier;
-    doc["flight_category"] = flight_category;
-    doc["ceiling"] = (ceiling.toInt() >= 50000 ? "None" : ceiling + " feet");
-    doc["wind_speed"] = String(wind_speed) + " knots";
-    doc["isDrinkingWeather"] = isDrinkingWeather(flight_category, wind_speed, ceiling);
-
-    serializeJson(doc, Serial);
-
-    // send response to client
-    client.print(header);
-    serializeJson(doc, client); 
-  } else if (request.indexOf("DRINKING") > 0) {
-    timerActive = false;
-    setDrinkingWeatherLights();
-    client.print(textHeader);
-    client.print("Drinking Weather");
-  } else if (request.indexOf("FLYING") > 0) {
-    timerActive = false;
-    setFlyingWeatherLights();
-    client.print(textHeader);
-    client.print("Flying Weather");
-  } else {
-    client.flush();
-    client.print(header);
-    client.print(html_1);
-    delay(5);
-  }
+  // check for client requests
+  Portal.handleClient();
+  MDNS.update();
 }
